e749dd3a9b8ebb8a0f581dce418dc7d735a59db4
MikeP <id2k1149@gmail.com>
2021-04-02 13:50:59 -0400

    oauth-msal-starter 02

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 class Config(object):
     # In a production app, store this instead in KeyVault or an environment variable
     # Enter your client secret from Azure AD below
-    CLIENT_SECRET = "H4.4k.si0KLP.I8Fa1xX1A.jV7gIA~s92z"
+    CLIENT_SECRET = "iDb~p91O7.yFHPq25_2UNHH83ZLe-D~U64"
 
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
     # Enter your application client ID here
-    CLIENT_ID = "63e32caf-0752-46a0-b7e3-6e89397a3a08"
+    CLIENT_ID = "9d72a26f-6c8b-4c3d-9a8c-34c51aa13299"
 
     # Enter the redirect path you want to use for OAuth requests
     #   Note that this will be the end of the URI entered back in Azure AD
